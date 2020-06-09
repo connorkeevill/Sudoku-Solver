9aacd8f760365f564c45d8c256cba94388cee3bd
Connor Keevill <connor.keevill.19@gmail.com>
2020-06-09 17:26:48 +0100

    Write failing test for solved boards

---
+++ src/SudokuSolver.java
@@ -0,0 +1,13 @@
+public class SudokuSolver {
+	private SudokuBoard board;
+
+	public SudokuSolver(SudokuBoard board)
+	{
+		this.board = board;
+	}
+
+	public SudokuBoard solve()
+	{
+		return this.board;
+	}
+}
