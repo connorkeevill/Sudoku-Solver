fee41cbc27ae8c1eaed54667544a86758883e7a4
Connor Keevill <connor.keevill.19@gmail.com>
2020-06-09 19:42:42 +0100

    Write code to solve sudoku and pass tests

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -6,8 +6,60 @@ public class SudokuSolver {
 		this.board = board;
 	}
 
-	public SudokuBoard solve()
+	public void solve()
 	{
-		return this.board;
+		recursiveSolve(0);
+	}
+
+	private boolean recursiveSolve(int startPoint)
+	{
+		int square = getIndexOfNextFreeSquare(startPoint);
+
+		// If we're at the final square and the board is valid then we're done.
+		if(square == 81 && board.isValid())
+		{
+			return true;
+		}
+		// If we're at the final square and the board isn't valid, then we need to go back
+		if(square == 81)
+		{
+			return false;
+		}
+
+		for(int numberToTry = 1; numberToTry <= 9; numberToTry++)
+		{
+			board.setValueAt(square, numberToTry);
+
+			if(!board.isValid())
+			{
+				continue;
+			}
+
+			if(recursiveSolve(square))
+			{
+				return true;
+			}
+		}
+
+		board.setValueAt(square, 0);
+		return false;
+	}
+
+	public SudokuBoard getBoard() {
+		return board;
+	}
+
+	/**
+	 * Finds the closest free square at or after the given square.
+	 * @param square the square to look at.
+	 * @return the closest free square.
+	 */
+	private int getIndexOfNextFreeSquare(int square) {
+		while(square < 81 &&!board.isSquareEmpty(square))
+		{
+			square++;
+		}
+
+		return square;
 	}
 }
